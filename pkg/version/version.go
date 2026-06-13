807159345528375a388422c99b2d5a4164ead57b
Moz <ariariari140090@gmail.com>
2026-06-13 17:47:25 +0330

    bump: version to 0.1.3

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // These variables are injected at build time via -ldflags.
 var (
-	Version   = "0.1.2"
+	Version   = "0.1.3"
 	Commit    = "none"
 	BuildDate = "unknown"
 	BuiltBy   = "source"
